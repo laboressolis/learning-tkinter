a564192fc055a0f36d68e4fe44a34e56e4160722
Glacier <zheanis15@gmail.com>
2024-05-06 13:09:18 +0800

    buttons and canvas

---
+++ button.py
@@ -0,0 +1,9 @@
+from tkinter import *
+root = Tk()
+
+button = Button(root, text='haiiii')
+button.pack()
+
+root.mainloop()
+
+# whats the difference b/w .pack() and .place()
---
+++ canvas.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+root = Tk(baseName='welp')
+
+canvas = Canvas(root,background='#FF00FF',)
+canvas.pack()
+
+root.mainloop()
